52e2fd3579af059bb1ba74f42f9f10f1713ca94e
Akash Prasher <prince.akash.prasher@gmail.com>
2022-01-31 19:27:15 +0530

    intital commit with a few tweeks
    Signed-off-by: Akash Prasher <prince.akash.prasher@gmail.com>

---
+++ understanding-basic-toolchain/src/components/app.js
@@ -0,0 +1,17 @@
+import React from "react";
+// import ReactDOM from "react-dom";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello World 💕!</h1>
+      <img
+        className="dog-img"
+        src="./images/img-1.jpg"
+        alt="dog-and-cat-together"
+      ></img>
+    </div>
+  );
+};
+
+export default App;
