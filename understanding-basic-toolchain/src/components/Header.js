ee4980e67bb5a02206b6c6baf8f2b439753e4d36
Akash Prasher <prince.akash.prasher@gmail.com>
2022-02-02 22:03:27 +0530

    adding got app
    Signed-off-by: Akash Prasher <prince.akash.prasher@gmail.com>

---
+++ understanding-basic-toolchain/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+let Header = () => (
+  <div>
+    <header>
+      <p>👑 Game of Thrones 👑</p>
+    </header>
+  </div>
+);
+
+export default Header;
--- understanding-basic-toolchain/src/components/app.js
+++ understanding-basic-toolchain/src/components/app.js
@@ -1,53 +1,13 @@
 import React from "react";
 // import ReactDOM from "react-dom";
+import Header from "./Header";
+import Tags from "./Tags";
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      counter: 0,
-    };
-  }
-
-  handleClick = (operation) => {
-    switch (operation) {
-      case "in":
-        this.setState({
-          counter: this.state.counter + 1,
-        });
-        break;
-
-      case "de":
-        this.setState({
-          counter: this.state.counter - 1,
-        });
-        break;
-
-      case "re":
-        this.setState({
-          counter: 0,
-        });
-        break;
-    }
-  };
-
-  render() {
-    return (
-      <center>
-        <h1>Counter App !</h1>
-        <h2 className="m-2">{this.state.counter}</h2>
-        <button onClick={() => this.handleClick("in")} className="btn-in">
-          Increment
-        </button>
-        <button onClick={() => this.handleClick("re")} className="btn-re">
-          Reset
-        </button>
-        <button onClick={() => this.handleClick("de")} className="btn-de">
-          Decrement
-        </button>
-      </center>
-    );
-  }
-}
+let App = () => (
+  <>
+    <Header />
+    <Tags />
+  </>
+);
 
 export default App;
