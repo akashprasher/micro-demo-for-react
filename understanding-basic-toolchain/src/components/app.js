2a97a135c27d4540e5c92cc75bf9f468556c11be
Akash Prasher <prince.akash.prasher@gmail.com>
2022-02-02 19:24:40 +0530

    setState
    Signed-off-by: Akash Prasher <prince.akash.prasher@gmail.com>

--- understanding-basic-toolchain/src/components/app.js
+++ understanding-basic-toolchain/src/components/app.js
@@ -1,17 +1,53 @@
 import React from "react";
 // import ReactDOM from "react-dom";
-import Header from "./Header";
-import Hero from "./Hero";
-import Articles from "./Articles";
-import Footer from "./Footer";
 
-const App = () => (
-  <>
-    <Header />
-    <Hero />
-    <Articles />
-    <Footer />
-  </>
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 0,
+    };
+  }
+
+  handleClick = (operation) => {
+    switch (operation) {
+      case "in":
+        this.setState({
+          counter: this.state.counter + 1,
+        });
+        break;
+
+      case "de":
+        this.setState({
+          counter: this.state.counter - 1,
+        });
+        break;
+
+      case "re":
+        this.setState({
+          counter: 0,
+        });
+        break;
+    }
+  };
+
+  render() {
+    return (
+      <center>
+        <h1>Counter App !</h1>
+        <h2 className="m-2">{this.state.counter}</h2>
+        <button onClick={() => this.handleClick("in")} className="btn-in">
+          Increment
+        </button>
+        <button onClick={() => this.handleClick("re")} className="btn-re">
+          Reset
+        </button>
+        <button onClick={() => this.handleClick("de")} className="btn-de">
+          Decrement
+        </button>
+      </center>
+    );
+  }
+}
 
 export default App;
