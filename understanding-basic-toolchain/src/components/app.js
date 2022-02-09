9f7bdfd58c6040494e847d31c37f0a6a026af68c
Akash Prasher <prince.akash.prasher@gmail.com>
2022-02-09 21:18:27 +0530

    refactoring lifting state approach
    Signed-off-by: Akash Prasher <prince.akash.prasher@gmail.com>

--- understanding-basic-toolchain/src/components/app.js
+++ understanding-basic-toolchain/src/components/app.js
@@ -1,13 +1,55 @@
 import React from "react";
-// import ReactDOM from "react-dom";
 import Header from "./Header";
 import Tags from "./Tags";
+import data from "../data.json";
+import People from "./People";
 
-let App = () => (
-  <>
-    <Header />
-    <Tags />
-  </>
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeHouse: "",
+    };
+  }
+
+  handleTagClick = (house) => {
+    this.setState({
+      activeHouse: house,
+    });
+  };
+
+  setDefault = () => {
+    this.setState({
+      activeHouse: "",
+    });
+  };
+
+  render() {
+    // let houseName = data.map((house) => house.name);
+    let everyone;
+    if (!this.state.activeHouse) {
+      everyone = data.reduce((dtAcc, house) => {
+        dtAcc = dtAcc.concat(house.people);
+        return dtAcc;
+      }, []);
+    } else {
+      everyone = data.find(
+        (house) => house.name === this.state.activeHouse
+      ).people;
+    }
+    return (
+      <>
+        <Header />
+        <Tags
+          activeHouse={this.state.activeHouse}
+          handleTagClick={this.handleTagClick}
+          setDefault={this.setDefault}
+          data={data}
+        />
+        <People everyone={everyone} />
+      </>
+    );
+  }
+}
 
 export default App;
