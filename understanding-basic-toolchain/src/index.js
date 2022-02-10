fb26f8d6fbf9b51ce57e9c73d2ddf9cd7a6071ae
Akash Prasher <prince.akash.prasher@gmail.com>
2022-02-10 14:46:09 +0530

    addng routes
    Signed-off-by: Akash Prasher <prince.akash.prasher@gmail.com>

--- understanding-basic-toolchain/src/index.js
+++ understanding-basic-toolchain/src/index.js
@@ -2,6 +2,29 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import App from "./components/app";
+import Header from "./components/Header";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Post from "./components/Post";
+import NotFound from "./components/NotFound";
 import "./styles/main.css";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+ReactDOM.render(
+  <BrowserRouter>
+    <div className="container">
+      <Header />
+    </div>
+    <Routes>
+      <Route path="/" exact element={<App />}></Route>
+      <Route path="/about" element={<About />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/post">
+        <Route path=":postId" element={<Post />} />
+      </Route>
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
